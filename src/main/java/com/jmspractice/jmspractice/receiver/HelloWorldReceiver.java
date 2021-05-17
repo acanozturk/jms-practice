3172eace6e94571052e3ff050f918ec8bdbb87f8
Ali Can Öztürk <acanozturk96@gmail.com>
2021-05-17 16:29:30 +0300

    Changed server properties, now running on docker

--- src/main/java/com/jmspractice/jmspractice/receiver/HelloWorldReceiver.java
+++ src/main/java/com/jmspractice/jmspractice/receiver/HelloWorldReceiver.java
@@ -6,12 +6,17 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class HelloWorldReceiver {
 
     @JmsListener(destination = JmsConfig.QUEUE)
     public void receiver(@Payload final HelloWorldMessage senderMessage) {
-        System.out.println("Message received: " + senderMessage);
+        final String message = senderMessage.getMessage();
+        final UUID uuid = senderMessage.getUuid();
+
+        System.out.println("Message received:" + message + ", UUID:" + uuid);
     }
 
 }
